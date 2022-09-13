6ef248c6db3140c680610661dd99a5e46b68731b
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-09-13 06:16:18 +0530

    okie

---
+++ React-Backup/class-3/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+
+    return <div>
+        <h1>Vikranth Rona</h1>
+        <h1>Bombay</h1>
+        <h1>Liger</h1>
+        <h1>Sita Rama...</h1>
+        <h1>Vikram...</h1>
+    </div>
+}
+export default App
---
+++ React-Backup/class-4/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+
+    return <div>
+        <h1>GM</h1>
+        <h2>GA</h2>
+        <h3>GE</h3>
+    </div>
+}
+export default App
